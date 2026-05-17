1dac1cd7374308a44bfcee2f32f3722c131e0356
va1bhava <dvaibhava2004@gmail.com>
2026-05-17 12:57:24 +0530

    Added otp validity function

--- src/main/java/com/uptimerobot/services/OtpService.java
+++ src/main/java/com/uptimerobot/services/OtpService.java
@@ -10,6 +10,7 @@ import sibModel.SendSmtpEmailTo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -21,38 +22,46 @@ public class OtpService {
     @Value("${BREVO_API_KEY}")
     private String brevoApiKey;
 
-    Map<String, String> otpStore = new ConcurrentHashMap<>();
+    record OtpEntry(String Otp, Instant expiresAt){}
+
+    Map<String, OtpEntry> otpStore = new ConcurrentHashMap<>();
+
 
     public void sendOtp(String email) throws Exception {
-        String otp = String.format("%06d", new Random().nextInt(999999));
-        otpStore.put(email, otp);
+        String otp = String.format("%06d", new Random().nextInt(99999));
 
-        ApiClient defaultClient = Configuration.getDefaultApiClient();
-        ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
-        apiKey.setApiKey(brevoApiKey);
+        Instant expiresAt=Instant.now().plusSeconds(180);
+        otpStore.put(email,new OtpEntry(otp,expiresAt));
+        ApiClient defaultClient=Configuration.getDefaultApiClient();
+        ApiKeyAuth apiKeyAuth=(ApiKeyAuth) defaultClient.getAuthentication("api-key");
+        apiKeyAuth.setApiKey(brevoApiKey);
 
-        TransactionalEmailsApi api = new TransactionalEmailsApi();
+        SendSmtpEmailSender sender=new SendSmtpEmailSender();
+        sender.setEmail("uptimerobot.auth@gmail.com");
+        sender.setName("UpTimeRobot");
 
-        SendSmtpEmailSender sender = new SendSmtpEmailSender();
-        sender.setEmail("uptimerobot.auth@gmail.com");  // your verified sender
-        sender.setName("UptimeRobot");
+        SendSmtpEmailTo recipent= new SendSmtpEmailTo();
+        recipent.setEmail(email);
 
-        SendSmtpEmailTo recipient = new SendSmtpEmailTo();
-        recipient.setEmail(email);
+        TransactionalEmailsApi transactionalEmailsApi= new TransactionalEmailsApi();
 
         SendSmtpEmail sendSmtpEmail = new SendSmtpEmail();
-        sendSmtpEmail.setSender(sender);
-        sendSmtpEmail.setTo(List.of(recipient));
-        sendSmtpEmail.setSubject("Your UptimeRobot OTP");
-        sendSmtpEmail.setTextContent("Your OTP is: " + otp + ". Valid for 10 mins!");
-
-        api.sendTransacEmail(sendSmtpEmail);
+        sendSmtpEmail.sender(sender);
+        sendSmtpEmail.setTo(List.of(recipent));
+        sendSmtpEmail.setSubject("Your OTP for registration is : ");
+        sendSmtpEmail.setTextContent(""+otp+ "\n\nValid for 3 minutes !");
+         transactionalEmailsApi.sendTransacEmail(sendSmtpEmail);
     }
 
     public boolean verifyOtp(String email, String otp) {
-        String stored = otpStore.get(email);
-        if (stored == null || !stored.equals(otp)) return false;
-        otpStore.remove(email);
-        return true;
+        OtpEntry entry=otpStore.get(email);
+        if(entry==null)return false;
+        Instant expiry= entry.expiresAt();
+        if(Instant.now().isAfter(expiry)){
+            otpStore.remove(email);
+            return false;
+        }
+         if(!entry.Otp().equals(otp))return false;
+         return true;
     }
 }
